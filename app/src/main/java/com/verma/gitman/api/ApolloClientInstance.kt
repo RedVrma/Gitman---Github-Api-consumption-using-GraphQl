17c56e08ecc7ce389f9da8a2f6a223143e6d9547
Puneet Verma <vpuneetwork@gmail.com>
2024-01-08 19:12:45 +0530

    removes token

--- app/src/main/java/com/verma/gitman/api/ApolloClientInstance.kt
+++ app/src/main/java/com/verma/gitman/api/ApolloClientInstance.kt
@@ -6,7 +6,8 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 
 object ApolloClientInstance {
-    val token = "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"
+    ///Add your token here
+    val token = ""
     val okHttpClient: OkHttpClient = OkHttpClient.Builder()
         .addInterceptor { chain: Interceptor.Chain ->
             val original: Request = chain.request()
